aa2a3e9ae5e549149296df2a5d3808901880ed4f
Maxime Duchene <duchene.maxime@outlook.fr>
2018-06-04 15:28:42 +0200

    TP 2.3 terminé

--- pub-services/services/pubs.services.js
+++ pub-services/services/pubs.services.js
@@ -1,13 +1,14 @@
 var pubs = require('../mocks/pubs.json');
+const moment = require('moment');
 
 function tousPubs(){
     return pubs;
 }
 
-function pubsOuverts(jour){
+function pubsOuverts(){
     var res = [];
     pubs.forEach(pub => {
-        if(pub['openDays'].includes(jour))
+        if(pub['openDays'].includes(moment().format('dddd')))
             res.push(pub['name']);
     });
     return res;
